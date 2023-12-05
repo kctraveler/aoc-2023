138b8ed12dba2baf58a4864bf0b436f730762542
Shane Panchot <shane.panchot@gmail.com>
2023-12-04 19:59:13 -0600

    clear target dirs

---
+++ day-three/src/main/java/com/kctraveler/aoc/DayThree.java
@@ -0,0 +1,15 @@
+package com.kctraveler.aoc;
+
+
+import java.util.ArrayList;
+
+public class DayThree {
+
+
+    public static void main(String[] args){
+        var in = DayTwo.getInput("day-three-input.txt")
+                .map(row -> row.split("")).toList();
+
+
+    }
+}
